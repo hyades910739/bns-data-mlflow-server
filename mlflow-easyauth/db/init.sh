3705916881ed26da614f60b51bef4dbf2a82425e
Eric <eric@admindeMacBook-Pro.local>
2023-07-20 12:38:40 +0800

    remove replicated files

--- mlflow-easyauth/db/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-mysql --protocol=socket -uroot -p$MYSQL_ROOT_PASSWORD <<EOSQL
-CREATE DATABASE mlflow IF NOT EXISTS;
-SET NAMES 'utf8';
-alter database mlflow character set utf8;
-EOSQL
--- mlflow-easyauth/db/sqls/init_db.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS mlflow;
-SET NAMES 'utf8';
-alter database mlflow character set utf8;
-create user 'eric'@'%' identified by '123';
-GRANT ALL privileges ON mlflow.* TO 'eric'@'%';
--- GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '' WITH GRANT OPTION;
